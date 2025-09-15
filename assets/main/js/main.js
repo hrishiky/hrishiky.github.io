0a949039e5779cc25f6a25daf887680361c6694a
Hrishik Yendluri <hrishik.yendluri@gmail.com>
2025-09-15 18:42:24 +0000

    includes

--- assets/main/js/main.js
+++ assets/main/js/main.js
@@ -5,3 +5,13 @@ function reveal() {
     const button = document.getElementById("hide-btn");
     button.style.display = "none";
 }
+
+function fillPlaceholders() {
+    fetch('/includes/nav.html')
+    .then(res => res.text())
+    .then(data => document.getElementById('nav-placeholder').innerHTML = data);
+
+  fetch('/includes/footer.html')
+    .then(res => res.text())
+    .then(data => document.getElementById('footer-placeholder').innerHTML = data);
+}
