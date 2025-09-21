d028149fd2c4ee1d2fcdcadb350846d90e7b6f09
Hrishik Yendluri <hrishik.yendluri@gmail.com>
2025-09-21 18:12:38 +0000

    navbar update and unique hits

--- assets/main/js/main.js
+++ assets/main/js/main.js
@@ -13,5 +13,18 @@ function fillPlaceholders() {
 
   fetch("/includes/footer.html")
     .then(res => res.text())
-    .then(data => document.getElementById("footer-placeholder").innerHTML = data);
+    .then(data => {
+      document.getElementById("footer-placeholder").innerHTML = data;
+
+      const cookies = document.cookie.split('; ').reduce((acc, cur) => {
+        const [key, val] = cur.split('=');
+        acc[key] = val;
+        return acc;
+      }, {});
+
+      if (!cookies["unique-counted"] || cookies["unique-counted"] === "false") {
+        document.getElementById("footer-placeholder").innerHTML += '<a class="counter" href="https://hits.sh/hrishiky.github.io/static/misc/stats/index.html/"><img src="https://hits.sh/hrishiky.github.io/static/misc/stats/index.html.svg"/></a>';
+        document.cookie = "unique-counted=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+      }
+    });
 }
